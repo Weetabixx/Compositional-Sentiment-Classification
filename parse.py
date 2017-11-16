cc3049329c0fe4f14e844240ccd46856398a4636
Weetabixx <robingraf@hotmail.co.uk>
2017-11-16 19:31:38 +0000

    added indentation to traverse function

--- parse.py
+++ parse.py
@@ -7,6 +7,15 @@ def prettyprint(tree):  # takes a list of parse trees and prettyprints them
 	for sub in tree:
 		sub.pretty_print()
 
+def traverse(tree, level=0):
+	for subtree in tree:
+		if type(subtree) == Tree:  # if not a leaf
+			print(("  "*level) + str(subtree.label()) + "(" )
+			traverse(subtree,(level+1))
+			print(("  "*level) + ")")
+		else:  # if leaf node
+			print(("  "*level) + subtree)
+
 
 parser=StanfordParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
 text = input()
@@ -16,3 +25,4 @@ sample = list(parser.raw_parse(text))
 # sample += list(parser.raw_parse(text2))
 
 prettyprint(sample)
+traverse(sample)
