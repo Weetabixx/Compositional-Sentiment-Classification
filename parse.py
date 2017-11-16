831ba87eb7246b78b191f4869868ab48c00ee99e
Weetabixx <robingraf@hotmail.co.uk>
2017-11-16 16:27:17 +0000

    changed pretty print

--- parse.py
+++ parse.py
@@ -1,12 +1,18 @@
 from nltk.parse.stanford import StanfordParser  # this does not work in pycharm IDE, must be ran in a terminal
 from nltk.tree import *
 import os
-parser=StanfordParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
-sample = list(parser.raw_parse("the quick brown fox jumps over the lazy dog"))
 
 
 def prettyprint(tree):  # takes a list of parse trees and prettyprints them
 	for sub in tree:
 		sub.pretty_print()
+
+
+parser=StanfordParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
+text = input()
+
+sample = list(parser.raw_parse(text))
+# text2 = input()
+# sample += list(parser.raw_parse(text2))
 
 prettyprint(sample)
