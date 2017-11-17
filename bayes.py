33c9d554720665f735b747965f9fc514d0efe2b1
Weetabixx <robingraf@hotmail.co.uk>
2017-11-17 08:34:27 +0000

    started work on bayes for comparison

---
+++ bayes.py
@@ -0,0 +1,142 @@
+import re, random, math, collections, itertools
+
+#------------- Function Definitions ---------------------
+
+def readFiles(sentimentDictionary,sentencesTrain,sentencesTest,sentencesNokia):
+
+    #reading pre-labeled input and splitting into lines
+    posSentences = open('rt-polarity.pos', 'r', encoding="ISO-8859-1")
+    posSentences = re.split(r'\n', posSentences.read())
+
+    negSentences = open('rt-polarity.neg', 'r', encoding="ISO-8859-1")
+    negSentences = re.split(r'\n', negSentences.read())
+
+    posSentencesNokia = open('nokia-pos.txt', 'r')
+    posSentencesNokia = re.split(r'\n', posSentencesNokia.read())
+
+    negSentencesNokia = open('nokia-neg.txt', 'r', encoding="ISO-8859-1")
+    negSentencesNokia = re.split(r'\n', negSentencesNokia.read())
+ 
+    posDictionary = open('positive-words.txt', 'r', encoding="ISO-8859-1")
+    posWordList = re.findall(r"[a-z\-]+", posDictionary.read())
+
+    negDictionary = open('negative-words.txt', 'r', encoding="ISO-8859-1")
+    negWordList = re.findall(r"[a-z\-]+", negDictionary.read())
+
+    for i in posWordList:
+        sentimentDictionary[i] = 1
+    for i in negWordList:
+        sentimentDictionary[i] = -1
+
+    #create Training and Test Datsets:
+    #We want to test on sentences we haven't trained on, to see how well the model generalses to previously unseen sentences
+
+  #create 90-10 split of training and test data from movie reviews, with sentiment labels    
+    for i in posSentences:
+        if random.randint(1,10)<2:
+            sentencesTest[i]="positive"
+        else:
+            sentencesTrain[i]="positive"
+
+    for i in negSentences:
+        if random.randint(1,10)<2:
+            sentencesTest[i]="negative"
+        else:
+            sentencesTrain[i]="negative"
+
+    #create Nokia Datset:
+    for i in posSentencesNokia:
+            sentencesNokia[i]="positive"
+    for i in negSentencesNokia:
+            sentencesNokia[i]="negative"
+
+#----------------------------End of data initialisation ----------------#
+
+#calculates p(W|Positive), p(W|Negative) and p(W) for all words in training data
+def trainBayes(sentencesTrain, pWordPos, pWordNeg, pWord):
+    posFeatures = [] # [] initialises a list [array]
+    negFeatures = [] 
+    freqPositive = {} # {} initialises a dictionary [hash function]
+    freqNegative = {}
+    dictionary = {}
+    posWordsTot = 0
+    negWordsTot = 0
+    allWordsTot = 0
+
+    #iterate through each sentence/sentiment pair in the training data
+    for sentence, sentiment in sentencesTrain.items():
+        wordList = re.findall(r"[\w']+", sentence)
+
+        #TO DO:
+        #Populate bigramList (initialised below) by concatenating adjacent words in the sentence.
+        #You might want to seperate the words by _ for readability, so bigrams such as:
+        #You_might, might_want, want_to, to_seperate.... 
+
+        bigramList=wordList #initialise bigramList
+        for x in range(len(wordList)-1):
+           bigramList.append(wordList[x]+"_" + wordList[x+1])
+
+ 
+        #-------------Finish populating bigramList ------------------#
+        
+        #TO DO: when you have populated bigramList, uncomment out the line below and , and comment out the unigram line to make use of bigramList rather than wordList
+        
+        for word in bigramList: #calculate over bigrams
+            # for word in wordList: #calculate over unigrams
+            allWordsTot += 1 # keeps count of total words in dataset
+            if not (word in dictionary):
+                dictionary[word] = 1
+            if sentiment=="positive" :
+                posWordsTot += 1 # keeps count of total words in positive class
+
+                #keep count of each word in positive context
+                if not (word in freqPositive):
+                    freqPositive[word] = 1
+                else:
+                    freqPositive[word] += 1    
+            else:
+                negWordsTot+=1# keeps count of total words in negative class
+                
+                #keep count of each word in positive context
+                if not (word in freqNegative):
+                    freqNegative[word] = 1
+                else:
+                    freqNegative[word] += 1
+
+    for word in dictionary:
+        #do some smoothing so that minimum count of a word is 1
+        if not (word in freqNegative):
+            freqNegative[word] = 1
+        if not (word in freqPositive):
+            freqPositive[word] = 1
+
+        # Calculate p(word|positive)
+        pWordPos[word] = freqPositive[word] / float(posWordsTot)
+
+        # Calculate p(word|negative) 
+        pWordNeg[word] = freqNegative[word] / float(negWordsTot)
+
+        # Calculate p(word)
+        pWord[word] = (freqPositive[word] + freqNegative[word]) / float(allWordsTot) 
+
+#---------------------------End Training ----------------------------------
+
+
+#---------- Main Script --------------------------
+
+
+sentimentDictionary={} # {} initialises a dictionary [hash function]
+sentencesTrain={}
+sentencesTest={}
+sentencesNokia={}
+
+#initialise datasets and dictionaries
+readFiles(sentimentDictionary,sentencesTrain,sentencesTest,sentencesNokia)
+
+pWordPos={} # p(W|Positive)
+pWordNeg={} # p(W|Negative)
+pWord={}    # p(W) 
+
+#build conditional probabilities using training data
+trainBayes(sentencesTrain, pWordPos, pWordNeg, pWord)
+
